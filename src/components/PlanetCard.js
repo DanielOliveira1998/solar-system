ba6284b65430772a0bbaa72d17fbc69efe54e202
Daniel Oliveira <danoliveira865@gmail.com>
2022-09-21 19:59:08 -0300

    Adicona os componentes PlanetCard e SolarSystem (requisito 5 e 6).

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -3,17 +3,18 @@ import PropTypes from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
-    const { name, image } = this.props;
+    const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
-        <p data-testid="planet-name" planetName={ name } planetImage={ image } />
+        <p data-testid="planet-name">{planetName}</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
 }
 
 PlanetCard.propTypes = {
-  name: PropTypes.string.isRequired, image: PropTypes.string.isRequired,
+  planetName: PropTypes.string.isRequired, planetImage: PropTypes.string.isRequired,
 };
 
 export default PlanetCard;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
-    return (<div data-testid="solar-system"><Title headline="Planetas" /></div>);
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+        {Planets.map(
+          (item, index) => (
+            <PlanetCard
+              key={ index }
+              planetName={ item.name }
+              planetImage={ item.image }
+            />
+          ),
+        )}
+      </div>
+    );
   }
 }
 
