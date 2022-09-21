d7b0e7cbd2ec3b665c1b2654526325394cb77fd6
Daniel Oliveira <danoliveira865@gmail.com>
2022-09-21 16:31:35 -0300

    Corrige erros lint.

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Planets from '../data/planets';
+// import React from 'react';
+// import Planets from '../data/planets';
 
-class PlanetCard extends React.Component {
-  render() {
-    // const { planetName } = this.props.name
-    return (<div>a</div>);
-  }
-}
+// class PlanetCard extends React.Component {
+//   render() {
+//     // const { planetName } = this.props.name
+//     return (<div>a</div>);
+//   }
+// }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// import Title from './Title';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
-    return (<div data-testid="solar-system">sistema solar div</div>);
+    return (<div data-testid="solar-system"><Title /></div>);
   }
 }
 
