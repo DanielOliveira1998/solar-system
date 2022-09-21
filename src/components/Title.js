f2d87e2e0badf942c8225aa085f2bd98739bc6bb
Daniel Oliveira <danoliveira865@gmail.com>
2022-09-21 15:40:46 -0300

    Corrige erros no lint.

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Title extends React.Component {
   render() {
-    return (<title { this.props.headlime } ></title>);
+    // const { headlime } = this.props.headlime;
+    return (<title><h2>{ headlime }</h2></title>);
   }
 }
+
+headlime.propTypes = { headlime: PropTypes.string };
+
+export default Title;
