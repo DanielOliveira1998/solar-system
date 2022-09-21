23f71b74d8aab81f3cb39d0f33a576e12199e855
Daniel Oliveira <danoliveira865@gmail.com>
2022-09-21 20:41:06 -0300

    Renderiza a lista lista de missões (requisito 10).

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import MissionCard from './MissionCard';
 import Title from './Title';
+import missions from '../data/missions';
 
 class Missions extends React.Component {
   render() {
-    return (<div data-testid="missions"><Title headline="Missões" /></div>);
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+        {missions.map(
+          (item, index) => (
+            <MissionCard
+              key={ index }
+              name={ item.name }
+              year={ item.year }
+              country={ item.country }
+              destination={ item.destination }
+            />
+          ),
+        )}
+      </div>
+    );
   }
 }
 
